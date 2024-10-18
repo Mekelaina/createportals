a26af96d70813ea6b60080fc4bccad21ec13b8bf
Mekelaina <chloerae018@gmail.com>
2024-10-18 17:03:40 -0400

    fixed rename issues with fabric.mod.json

--- common/src/main/java/com/mekelaina/createportals/CreatePortalsBlocks.java
+++ common/src/main/java/com/mekelaina/createportals/CreatePortalsBlocks.java
@@ -2,12 +2,17 @@ package com.mekelaina.createportals;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 
 public class CreatePortalsBlocks {
 	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(CreatePortals.MOD_ID);
 
-	public static final BlockEntry<Block> EXAMPLE_BLOCK = REGISTRATE.block("example_block", Block::new).register();
+	public static final BlockEntry<Block> EXAMPLE_BLOCK = REGISTRATE.block("example_block", Block::new)
+			.simpleItem()
+			.register();
+
 
 	public static void init() {
 		// load the class and register everything
