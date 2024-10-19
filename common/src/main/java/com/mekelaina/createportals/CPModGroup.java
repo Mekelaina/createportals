c7bd5a0b1296532223fb8fb98463a5613be8b25b
Mekelaina <chloerae018@gmail.com>
2024-10-18 23:06:18 -0400

    cant import dev.architectury.registy

---
+++ common/src/main/java/com/mekelaina/createportals/CPModGroup.java
@@ -0,0 +1,10 @@
+package com.mekelaina.createportals;
+
+
+import net.minecraft.world.item.CreativeModeTab;
+//import dev.architectury.registry
+//import dev.architectury.
+public class CPModGroup {
+
+  //  public static final CreativeModeTab TAB = CreativeTabsRegistry.create();
+}
