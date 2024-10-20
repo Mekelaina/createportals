54b38f61b489c7201e7c5416e90cac34a8191fc0
Mekelaina <chloerae018@gmail.com>
2024-10-19 21:40:32 -0400

    added entanglement item

---
+++ common/src/main/java/com/mekelaina/createportals/items/EntanglementItem.java
@@ -0,0 +1,10 @@
+package com.mekelaina.createportals.items;
+
+import net.minecraft.world.item.Item;
+
+public class EntanglementItem extends Item {
+
+    public EntanglementItem(Properties properties) {
+        super(properties);
+    }
+}
